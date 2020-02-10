56fde900da754485423254d29a1413d2bf3865e8
Cynicalsocks <47702497+Cynicalsocks@users.noreply.github.com>
2020-02-10 12:57:03 -0600

    some stuff

---
+++ 2D-Group-Project/Assets/Monster_Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class Monster_Controller : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public void Update()
+    {
+        {
+            TileBase tile = null;
+            tile = tilemap.GetTile(new Vector3Int(0, 0, 0));
+        }
+    }
+}
