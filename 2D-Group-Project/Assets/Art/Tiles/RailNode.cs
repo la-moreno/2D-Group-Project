6dddba54376a9347e361bc946b80e23a042a729f
Cynicalsocks <47702497+Cynicalsocks@users.noreply.github.com>
2020-02-12 15:25:33 -0600

    started pathfinding

---
+++ 2D-Group-Project/Assets/Art/Tiles/RailNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailNode
+{
+    Vector2Int gridPosition;
+    float hCost, fCost, gCost;
+
+    void Start()
+    {
+        
+    }
+}
