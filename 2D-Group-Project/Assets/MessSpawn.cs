3f5b7a72786807125cc86ad874d9028b47dc50bd
Amazingamer <60017816+Amazingamer@users.noreply.github.com>
2020-03-02 15:38:04 -0600

    work

--- 2D-Group-Project/Assets/MessSpawn.cs
+++ 2D-Group-Project/Assets/MessSpawn.cs
@@ -9,59 +9,63 @@ public class MessSpawn : MonoBehaviour
 {
     public int NumOfMesses;
     public GameObject[] Messes;
-    
+    public float waitTime = 5.0f;
+    private float timer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        InvokeRepeating("RandomMessPos", 10.0f, 10.0f );
+        timer += Time.deltaTime;
+        if (timer > waitTime && NumOfMesses <= 6)
+        {
+            RandomMessPos();
+            timer = timer - waitTime;
+        }
+
     }
 
     void RandomMessPos()
     {
         Vector3 MessSpawnPoint = new Vector3(transform.position.x, transform.position.y, 0);
-        
 
-        if (NumOfMesses <= 6)
+
+
+        int Index = MessNumb();
+        switch (Index)
         {
-            int Index = MessNumb();
-            switch (Index)
-            {
-                case 0:
-                    {
-                        Instantiate(Messes[Index], MessSpawnPoint, Quaternion.identity);
-                        NumOfMesses++;
-                        return;
-                    }
-                case 1:
-                    {
-                        Instantiate(Messes[Index], MessSpawnPoint, Quaternion.identity);
-                        NumOfMesses++;
-                        return;
-                    }
-                case 2:
-                    {
-                        Instantiate(Messes[Index], MessSpawnPoint, Quaternion.identity);
-                        NumOfMesses++;
-                        return;
-                    }
-               
-                default:
-                    break;
-            }
+            case 0:
+                {
+                    Instantiate(Messes[Index], MessSpawnPoint, Quaternion.identity);
+                    NumOfMesses++;
+                    return;
+                }
+            case 1:
+                {
+                    Instantiate(Messes[Index], MessSpawnPoint, Quaternion.identity);
+                    NumOfMesses++;
+                    return;
+                }
+            case 2:
+                {
+                    Instantiate(Messes[Index], MessSpawnPoint, Quaternion.identity);
+                    NumOfMesses++;
+                    return;
+                }
+
+            default:
+                break;
         }
-
-
     }
 
     int MessNumb()
     {
-       int MessNumber = Random.Range(0 , 5);
-       return MessNumber; 
+        int MessNumber = Random.Range(0, 5);
+        return MessNumber;
     }
 }
