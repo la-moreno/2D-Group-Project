eb1b12811f04dea1fb13df52158d37a177e6d410
Cynicalsocks <lucasamosmoreno@yahoo.com>
2020-03-04 23:49:01 -0600

    messes manager script

---
+++ 2D-Group-Project/Assets/Scripts/Furniture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furniture : MonoBehaviour
+{
+    public float dirtyValue = 0.0f;
+
+    Material material;
+    MessManager messManager;
+    void Start()
+    {
+        messManager = MessManager.Instance;
+        material = GetComponent<SpriteRenderer>().material;
+
+        // Add to list of furniture
+        messManager.furniture.Add(this);
+    }
+
+    private void Update()
+    {
+        material.SetFloat("DirtyValue", dirtyValue);
+    }
+}
