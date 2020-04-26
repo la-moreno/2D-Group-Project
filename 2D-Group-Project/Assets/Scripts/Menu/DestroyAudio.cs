6a9e9800ed7796627c215c203e26ebc44cdda7e7
Kurimbah <Kurimbah@gmail.com>
2020-04-25 19:18:25 -0500

    Main and Pause Menu Finished
    Main menu has animation and intro music.
    Pause menu has back to main menu and exit game options.
    Changed the camera view in the level so it doesn't look strange after it's built.
    Deleted a couple of tiles leading to the bedroom.
    
    (Menus may look strange in the Unity project, but they look fine in the build)

---
+++ 2D-Group-Project/Assets/Scripts/Menu/DestroyAudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAudio : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject audio = GameObject.Find("AudioManger");
+        if (audio != null)
+            Destroy(audio); 
+       
+    }
+}
---
+++ 2D-Group-Project/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PauseMenu : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject inventoryUI;
+    private Scene scene;
+    public static bool gameIsPaused;
+    public GameObject pauseMenuUI;
+   
+    void Start()
+    {
+        inventoryUI = GameObject.Find("Inventory");
+        player = GameObject.FindGameObjectWithTag("Player"); 
+        scene = SceneManager.GetActiveScene();
+        gameIsPaused = false;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (gameIsPaused)
+                    Resume();
+                else
+                    Pause(); 
+            }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        player.GetComponent<PlayerController>().enabled = true;
+        player.GetComponent<PlayerAttack>().enabled = true;
+        inventoryUI.SetActive(true); 
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        gameIsPaused = true;
+        pauseMenuUI.SetActive(true);
+        player.GetComponent<PlayerController>().enabled = false;
+        player.GetComponent<PlayerAttack>().enabled = false;
+        inventoryUI.SetActive(false); 
+        Time.timeScale = 0f;
+        
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Leaving Game");
+        Application.Quit();
+    }
+
+}
